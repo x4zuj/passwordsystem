a75f24fad6c63c7df2273624fd89b394f39abee2
x4zuj <nikita.kern09@web.de>
2026-03-29 15:30:31 +0200

    Add files via upload

---
+++ password_function.py
@@ -0,0 +1,18 @@
+
+# Welcome to my simple password_funtion.py
+# This is just a simple login system made by 4zuj.
+
+# First of All, I need variables, else this won't work.
+
+password = "required"
+user_input = input("Welcome User, who is logging in?: ") # Just some decoration c:
+
+while password == "required":
+    print(user_input,"Enter your password. ")
+    password_input = input("Password: ")
+    if password_input == "Test123":
+        print("Successfully in!")
+        input("Press ENTER or Ctrl+C to exit the program.")
+        exit()
+    else:
+        print("The password is incorrect. Please try it again. ")
